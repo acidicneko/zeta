564b6dd9740a684c6674073f1b4f63f3517296ca
acidicneko <ayush7788899@gmail.com>
2022-10-23 09:06:43 +0530

    change: change versioning scheme

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char** argv){
         std::cout << "Usage: zeta [--help] [--version] [init [language]] [build] [clean]" << std::endl;
         return 0;
     } else if (!strcmp(argv[1], "--version")){
-        std::cout << "Zeta C/C++ Helper Tool\nVersion: 0.0.2\n" 
+        std::cout << "Zeta C/C++ Helper Tool\nVersion: 1.0.0\n" 
                   << "Distributed under GNU GPLv3\n"
                   << "Copyright Ayush Yadav(acidicneko) 2022"<< std::endl;
         return 0;
