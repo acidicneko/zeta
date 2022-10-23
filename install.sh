c8325d1e362d5a3429435ecd22bc82bf45e79923
acidicneko <ayush7788899@gmail.com>
2022-10-23 09:33:08 +0530

    fix: add sudo copy command

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ echo Starting Zeta installation...
 sleep 3
 git clone https://github.com/acidicneko/zeta.git
 make -C zeta/
-cp zeta/build/zeta /usr/bin/zeta
+sudo cp zeta/build/zeta /usr/bin/zeta
 mkdir -p ~/.config/zeta
 cp -r zeta/assets/templates ~/.config/zeta/
 echo Zeta has been installed.
