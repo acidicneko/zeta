4016e30031cab961085e0f90255a0332f262cf3a
acidicneko <ayush7788899@gmail.com>
2024-02-01 10:37:38 +0530

    add: update function in gitman script

--- gitman_install.sh
+++ gitman_install.sh
@@ -17,10 +17,24 @@ uninstall() {
 	rm -rf ~/.config/zeta
 }
 
+update() {
+	make -j $(nproc)
+	mv build/zeta "$GITMAN_BIN"/zeta
+	cp -r assets/* ~/.config/zeta/
+	CURRENT_HASH=$(curl -H "Accept: application/vnd.github+json" \
+        	https://api.github.com/repos/acidicneko/zeta/commits/main \
+        	| grep sha \
+        	| awk 'NR==1{print $2}' \
+        	| sed 's/[",]//g')
+	echo "LAST_HASH=$CURRENT_HASH" > "$HOME"/.config/zeta/last_commit
+}
+
 if [ $1 = "install" ] ; then 
 	install
 elif [ $1 = "uninstall" ] ; then
 	uninstall
+elif [ $1 = "update" ] ; then
+	update
 else
 	echo unknown option
 	exit 1
